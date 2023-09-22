407a5a536963a849664704ad3b98e7a59ad1e5ae
gyorkistamas <tamasgyorkis@gmail.com>
2023-09-22 15:38:40 +0200

    add models

--- thesisproject/app/Models/User.php
+++ thesisproject/app/Models/User.php
@@ -6,11 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Ladder\HasRoles;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    protected $primaryKey = "id";
 
     /**
      * The attributes that are mass assignable.
--- thesisproject/database/migrations/2023_09_22_130759_create_subjects_table.php
+++ thesisproject/database/migrations/2023_09_22_130759_create_subjects_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->string("id")->primary();
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->string('id')->primary();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('picture')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description')->nullable();
+            $table->integer('credit');
+            $table->string('manager');
+            $table->foreign('manager')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('subjects');
     }
 };

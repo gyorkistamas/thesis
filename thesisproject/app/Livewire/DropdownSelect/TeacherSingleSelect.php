bedc88c50c4af8e3408eb07655297611c26bb5a8
gyorkistamas <tamasgyorkis@gmail.com>
2024-01-29 16:53:39 +0100

    add dropdown selection with search

---
+++ thesisproject/app/Livewire/DropdownSelect/TeacherSingleSelect.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Livewire\DropdownSelect;
+
+use App\Models\User;
+use Livewire\Component;
+
+class TeacherSingleSelect extends Component
+{
+    public $query = '';
+
+    public $data;
+
+    public $selected_items = [];
+
+    public function updatedQuery()
+    {
+        $this->data = User::whereHas('roles', function ($query) {
+            $query->where('role', 'teacher');
+        })
+            ->where(function ($query) {
+                $query->where('name', 'like', '%'.$this->query.'%')
+                    ->orWhere('neptun', 'like', '%'.$this->query.'%');
+            })
+            ->get()
+            ->toArray();
+    }
+
+    public function addSelectedItem($user_id)
+    {
+        if (! empty($this->selected_items)) {
+            return;
+        }
+        $user = User::findOrFail($user_id, ['id', 'name', 'neptun']);
+
+        if (! empty($this->selected_items)) {
+            if (! in_array($user['id'], array_column($this->selected_items, 'id'))) {
+                $this->selected_items[] = $user;
+                $this->query = '';
+            }
+        } else {
+            $this->selected_items[] = $user;
+            $this->query = '';
+        }
+
+        $this->dispatch('single-select-teacher', $this->selected_items);
+    }
+
+    public function removeSelectedItem($id)
+    {
+        foreach ($this->selected_items as $key => $item) {
+            if ($item['id'] == $id) {
+                unset($this->selected_items[$key]);
+                break;
+            }
+        }
+        $this->dispatch('single-select-teacher', $this->selected_items);
+    }
+
+    public function resetProps()
+    {
+        $this->reset(['query', 'data']);
+    }
+
+    public function render()
+    {
+        return view('livewire.dropdown-select.teacher-single-select');
+    }
+}

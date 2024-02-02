4d4dab1fde523388b1d7718aa6100c05f043c72e
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-02 13:39:51 +0100

    list and create places

---
+++ thesisproject/app/Livewire/Administration/PlacesList.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Livewire\Administration;
+
+use App\Models\Place;
+use Auth;
+use Livewire\Component;
+use Livewire\WithPagination;
+use Usernotnull\Toast\Concerns\WireToast;
+
+class PlacesList extends Component
+{
+    use WireToast, WithPagination;
+
+    public $newName;
+
+    public function newPlace()
+    {
+        if (Auth::user()->cannot('create', Place::class)) {
+            toast()->danger(__('general.noPermission'), __('general.error'))->push();
+
+            return;
+        }
+
+        $this->validate([
+            'newName' => 'required|string|max:255',
+        ]);
+
+        Place::create([
+            'name' => $this->newName,
+        ]);
+
+        $this->newName = '';
+        toast()->success(__('general.createNewPlace'), __('general.success'))->push();
+
+    }
+
+    public function render()
+    {
+        $places = Place::paginate(10, pageName: 'placesPage');
+        return view('livewire.administration.places-list')->with('places', $places);
+    }
+}
---
+++ thesisproject/resources/views/livewire/administration/places-list.blade.php
@@ -0,0 +1,69 @@
+<div>
+    <div class="fixed inset-0 flex items-center justify-center" style="pointer-events: none;">
+        <span class="loading loading-dots loading-lg" wire:loading></span>
+    </div>
+    <div class="prose mb-3 flex flex-row flex-wrap justify-between min-w-full max-w-full md:flex-row">
+        <h1 class="mb-0 mx-auto md:mx-0 md:ms-1">{{__('general.places')}}</h1>
+        <button class="btn btn-success w-fit" onclick="newPlaceModal.showModal()">
+            <x-icons.plus_fill_small/>{{__('general.createNewPlace')}}</button>
+    </div>
+    <div>
+        @if($places->count() != 0)
+            <div>
+                <table class="table">
+                    <!-- head -->
+                    <thead>
+                    <tr>
+                        <th>{{__('general.name')}}</th>
+                        <th>{{__('general.actions')}}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <!-- rows -->
+                    @foreach($places as $place)
+                       <livewire:administration.place-item-list :place="$place" :key="$place->id" />
+                    @endforeach
+                    </tbody>
+                    <!-- foot -->
+                    <tfoot>
+                    <tr>
+                        <th>{{__('general.name')}}</th>
+                        <th>{{__('general.actions')}}</th>
+                    </tr>
+                    </tfoot>
+
+                </table>
+            </div>
+            {{$places->links()}}
+        @else
+            <div class="prose mx-auto mt-2">
+                <h1>{{__('general.searchNotFound')}}</h1>
+            </div>
+        @endif
+    </div>
+
+    <dialog id="newPlaceModal" class="modal modal-bottom sm:modal-middle" wire:ignore.self>
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">{{__('general.createNewPlace')}}</h3>
+            <div class="modal-action flex flex-col">
+                <form class="">
+                    <div class="flex flex-col items-center">
+                        <div>
+                            <label for="name" class="label w-min">{{__('general.name')}}: </label>
+                            <input type="text" name="name" class="input input-bordered input-accent w-full max-w-xs" wire:model="newName"/>
+                        </div>
+                        @error('newName')
+                        <x-error-alert class="mt-3 mx-5">{{$message}}</x-error-alert>
+                        @enderror
+                    </div>
+                </form>
+                <div class="flex flex-row gap-3 mt-5 justify-end">
+                    <button class="btn btn-success" wire:click="newPlace">{{__('general.createNewPlace')}}</button>
+                    <form method="dialog">
+                        <button class="btn">{{__('general.close')}}</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </dialog>
+</div>

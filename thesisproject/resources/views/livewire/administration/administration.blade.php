93b5becc7a9ba8496d1582146999aab9d67e890a
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-02 13:40:19 +0100

    add places tab

--- thesisproject/resources/views/livewire/administration/administration.blade.php
+++ thesisproject/resources/views/livewire/administration/administration.blade.php
@@ -11,6 +11,18 @@
         @endif
     </div>
 
+
+    <input type="radio" name="administration" role="tab" class="tab" aria-label="{{__('general.places')}}"
+           value="places" wire:model.live="selectedTab"/>
+    <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+        <div class="fixed inset-0 flex items-center justify-center" style="pointer-events: none;">
+            <span class="loading loading-dots loading-lg" wire:loading></span>
+        </div>
+        @if($selectedTab == 'places')
+            <livewire:administration.places-list wire:key="place"/>
+        @endif
+    </div>
+
     <input type="radio" name="administration" role="tab" class="tab" aria-label="{{__('general.subjects')}}"
            value="subjects" wire:model.live="selectedTab"/>
     <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">

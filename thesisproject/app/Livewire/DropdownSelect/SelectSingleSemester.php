288fb661b4d00c442dbf5bd255222759897e1f9b
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-02 14:35:59 +0100

    added single semester selection

---
+++ thesisproject/app/Livewire/DropdownSelect/SelectSingleSemester.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Livewire\DropdownSelect;
+
+use App\Models\Term;
+use Livewire\Component;
+
+class SelectSingleSemester extends Component
+{
+    public $query = '';
+
+    public $data;
+
+    public $selected_items = [];
+
+    public $courseId;
+
+    public function updatedQuery()
+    {
+        $this->data = Term::where('name', 'like', '%'.$this->query.'%')
+            ->orderBy('start', 'desc')
+            ->get()
+            ->toArray();
+    }
+
+    public function addSelectedItem($term_id)
+    {
+        if (! empty($this->selected_items)) {
+            return;
+        }
+        $term = Term::findOrFail($term_id, ['id', 'name']);
+
+        if (! empty($this->selected_items)) {
+            if (! in_array($term['id'], array_column($this->selected_items, 'id'))) {
+                $this->selected_items[] = $term;
+                $this->query = '';
+            }
+        } else {
+            $this->selected_items[] = $term;
+            $this->query = '';
+        }
+
+        $this->dispatch('single-select-term.'.$this->courseId, data: $term->id);
+    }
+
+    public function removeSelectedItem($id)
+    {
+        foreach ($this->selected_items as $key => $item) {
+            if ($item['id'] == $id) {
+                unset($this->selected_items[$key]);
+                break;
+            }
+        }
+        $this->dispatch('single-select-term.'.$this->courseId, data: '');
+    }
+
+    public function resetProps()
+    {
+        $this->reset(['query', 'data']);
+    }
+
+    public function mount($selectedId, $courseId, $autoSelect = false)
+    {
+        $this->courseId = $courseId;
+
+        if ($autoSelect) {
+            foreach (Term::all() as $term) {
+                if ($term->active()) {
+                    $selectedId = $term->id;
+                    break;
+                }
+            }
+        }
+
+        if ($selectedId != null) {
+            $this->selected_items[] = Term::findOrFail($selectedId, ['id', 'name']);
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.dropdown-select.select-single-semester');
+    }
+}
---
+++ thesisproject/resources/views/livewire/dropdown-select/select-single-semester.blade.php
@@ -0,0 +1,52 @@
+<div>
+    <div class="relative">
+        <input
+            type="search"
+            class="input input-bordered input-accent w-full"
+            placeholder="{{__('general.search')}}..."
+            wire:model.live="query"
+            wire:ignore
+        />
+        <span wire:loading class="flex absolute right-0 loading loading-spinner loading-md mt-3 me-[13px]">
+        </span>
+
+        @if(!empty($query))
+            <div
+                class="absolute z-10 list-group w-full rounded-t-none shadow-lg">
+                <div class="block">
+                    <div class="absolute z-40 left-0 mt-2 w-full overflow-visible">
+                        <div class="py-1 text-sm rounded shadow-lg border input-bordered input-accent bg-base-300">
+                            @if(!empty($data))
+                                @foreach($data as $i => $item)
+                                    <a wire:click="addSelectedItem({{$item['id']}})"
+                                       class="block py-1 px-5 cursor-pointer hover:bg-accent hover:text-black font-semibold">{{$item['name']}}</a>
+                                @endforeach
+                            @else
+                                <span
+                                    class="block py-1 px-5 hover:bg-accent hover:text-black font-semibold">{{__('general.noResult')}}</span>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+    </div>
+
+    <!-- selections -->
+    @if(!empty($selected_items))
+        @foreach($selected_items as $selected_item)
+            <div class="inline-flex items-center text-sm rounded mt-2 mr-1 badge badge-success py-4 px-2">
+                <span
+                    class="ml-2 mr-1 leading-relaxed truncate max-w-xs">{{$selected_item['name']}}</span>
+                <button wire:click="removeSelectedItem({{$selected_item['id']}})" type="button"
+                        class="w-6 h-8 inline-block align-middle text-gray-500 hover:text-gray-600 focus:outline-none">
+                    <svg class="w-6 h-6 fill-current mx-auto" xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                              d="M15.78 14.36a1 1 0 0 1-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 1 1-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 0 1 1.42-1.42l2.83 2.83 2.82-2.83a1 1 0 0 1 1.42 1.42l-2.83 2.83 2.83 2.82z"/>
+                    </svg>
+                </button>
+            </div>
+        @endforeach
+    @endif
+</div>

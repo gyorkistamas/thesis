7d4ebf97c4f8f93f2a71d570099fd75295b3311a
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-05 14:18:31 +0100

    format

--- thesisproject/resources/views/livewire/administration/course-list-item.blade.php
+++ thesisproject/resources/views/livewire/administration/course-list-item.blade.php
@@ -19,7 +19,8 @@
                     <h3 class="font bold text-error">{{__('general.courseDeleteConfirm')}}</h3>
                     <div class="modal-action">
                         <form method="dialog">
-                            <button class="btn btn-error" wire:click="deleteCourse">{{__('general.courseDelete')}}</button>
+                            <button class="btn btn-error"
+                                    wire:click="deleteCourse">{{__('general.courseDelete')}}</button>
                             <button class="btn">{{__('general.close')}}</button>
                         </form>
                     </div>
@@ -49,42 +50,51 @@
                                 @if($currentTab == 'edit')
                                     <div class="">
                                         <label for="courseCode" class="label mt-2">{{__('general.courseId')}}</label>
-                                        <input type="text" name="courseCode" class="input input-bordered input-accent w-full max-w-2xl"
+                                        <input type="text" name="courseCode"
+                                               class="input input-bordered input-accent w-full max-w-2xl"
                                                wire:model="newCourseCode"/>
                                         @error('newCourseCode')
                                         <x-error-alert class="mt-2">{{$message}}</x-error-alert>
                                         @enderror
 
-                                        <label for="courseDescription" class="label mt-2">{{__('general.courseDescription')}}</label>
-                                        <input type="text" name="courseDescription" class="input input-bordered input-accent w-full max-w-2xl"
+                                        <label for="courseDescription"
+                                               class="label mt-2">{{__('general.courseDescription')}}</label>
+                                        <input type="text" name="courseDescription"
+                                               class="input input-bordered input-accent w-full max-w-2xl"
                                                wire:model="newCourseDescription"/>
                                         @error('newCourseDescription')
                                         <x-error-alert class="mt-2">{{$message}}</x-error-alert>
                                         @enderror
 
-                                        <label for="courseLimit" class="label mt-2">{{__('general.courseLimit')}}</label>
-                                        <input type="number" name="courseLimit" class="input input-bordered input-accent w-full max-w-2xl"
+                                        <label for="courseLimit"
+                                               class="label mt-2">{{__('general.courseLimit')}}</label>
+                                        <input type="number" name="courseLimit"
+                                               class="input input-bordered input-accent w-full max-w-2xl"
                                                wire:model="newCourseLimit"/>
                                         @error('newCourseLimit')
                                         <x-error-alert class="mt-2">{{$message}}</x-error-alert>
                                         @enderror
 
                                         <label for="teachers" class="label mt-2">{{__('general.teachers')}}</label>
-                                        <livewire:dropdown-select.teacher-multi-select :key="'editTeacherSelection'.$course->id"
-                                                                                       :selectedIds="$newCourseTeachers" :courseId="$course->id" class="w-full max-w-2xl"/>
+                                        <livewire:dropdown-select.teacher-multi-select
+                                            :key="'editTeacherSelection'.$course->id"
+                                            :selectedIds="$newCourseTeachers" :courseId="$course->id"
+                                            class="w-full max-w-2xl"/>
                                         @error('newCourseTeachers')
                                         <x-error-alert class="mt-2">{{$message}}</x-error-alert>
                                         @enderror
 
                                         <label for="semester" class="label mt-2">{{__('general.semester')}}</label>
-                                        <livewire:dropdown-select.select-single-semester :key="'editSemesterSelection'.$course->id"
-                                                                                         :selectedId="$newCourseSemester" :courseId="$course->id"
-                                                                                         :autoSelect="false" class="w-full max-w-2xl"/>
+                                        <livewire:dropdown-select.select-single-semester
+                                            :key="'editSemesterSelection'.$course->id"
+                                            :selectedId="$newCourseSemester" :courseId="$course->id"
+                                            :autoSelect="false" class="w-full max-w-2xl"/>
                                         @error('newCourseSemester')
                                         <x-error-alert class="mt-2">{{$message}}</x-error-alert>
                                         @enderror
 
-                                        <button class="btn btn-success mt-3" wire:click="editCourse"><x-icons.edit_fill_small />{{__('general.courseEdit')}}</button>
+                                        <button class="btn btn-success mt-3" wire:click="editCourse">
+                                            <x-icons.edit_fill_small/>{{__('general.courseEdit')}}</button>
                                     </div>
 
                                 @endif
@@ -108,35 +118,42 @@
                                         <tbody>
                                         <!-- rows -->
                                         @foreach($course->Classes as $class)
-                                            <livewire:administration.class-table-item :key="'classItem'.$course->id.$class->id" :class="$class"/>
+                                            <livewire:administration.class-table-item
+                                                :key="'classItem'.$course->id.$class->id" :class="$class"/>
                                         @endforeach
                                         <tr>
                                             <td>
-                                                <input type="datetime-local" name="startTimeNew" class="input input-bordered input-accent w-full max-w-2xl"
+                                                <input type="datetime-local" name="startTimeNew"
+                                                       class="input input-bordered input-accent w-full max-w-2xl"
                                                        wire:model="newClassStart"/>
                                                 @error('newClassStart')
                                                 <x-error-alert class="mt-2">{{$message}}</x-error-alert>
                                                 @enderror
                                             </td>
                                             <td>
-                                                <input type="datetime-local" name="endTimeNew" class="input input-bordered input-accent w-full max-w-2xl"
+                                                <input type="datetime-local" name="endTimeNew"
+                                                       class="input input-bordered input-accent w-full max-w-2xl"
                                                        wire:model="newClassEnd"/>
                                                 @error('newClassEnd')
                                                 <x-error-alert class="mt-2">{{$message}}</x-error-alert>
                                                 @enderror
                                             </td>
                                             <td>
-                                                <livewire:dropdown-select.single-place-select :key="'newPlaceSelection'.$course->id"
-                                                                                              :selectedId="null" :classId="-1" class="w-full max-w-2xl"/>
+                                                <livewire:dropdown-select.single-place-select
+                                                    :key="'newPlaceSelection'.$course->id"
+                                                    :selectedId="null" :classId="-1" class="w-full max-w-2xl"/>
                                                 @error('newClassPlace')
                                                 <x-error-alert class="mt-2">{{$message}}</x-error-alert>
                                                 @enderror
                                             </td>
                                             <td>
-                                                <button class="btn btn-success btn-sm" wire:click="newClass"><x-icons.plus_fill_small />{{__('general.add')}}</button>
+                                                <button class="btn btn-success btn-sm" wire:click="newClass">
+                                                    <x-icons.plus_fill_small/>{{__('general.add')}}</button>
                                                 <label class="cursor-pointer label">
-                                                    <span class="label-text">{{__('general.repeatUntilEndOfTermEveryWeek')}}</span>
-                                                    <input type="checkbox" class="checkbox checkbox-accent" wire:model="repeatUntilEndOfTerm"/>
+                                                    <span
+                                                        class="label-text">{{__('general.repeatUntilEndOfTermEveryWeek')}}</span>
+                                                    <input type="checkbox" class="checkbox checkbox-accent"
+                                                           wire:model="repeatUntilEndOfTerm"/>
                                                 </label>
                                             </td>
                                         </tr>
@@ -160,7 +177,48 @@
                                    wire:model.live="currentTab"/>
                             <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
                                 @if($currentTab == 'students')
+                                    <div class="flex flex-row gap-3 mb-4">
+                                        <livewire:dropdown-select.select-multiple-students-to-add-to-course
+                                            :key="'studentSelection'.$course->id"
+                                            :courseId="$course->id" :selectedStudents="$course->Students->pluck('id')->toArray()" />
 
+                                        <button class="btn btn-success" wire:click="addStudents">
+                                            <x-icons.plus_fill_small/>{{__('general.add')}}</button>
+                                    </div>
+
+                                    <table class="table">
+                                        <!-- head -->
+                                        <thead>
+                                        <tr>
+                                            <th>{{__('general.neptunCode')}}</th>
+                                            <th>{{__('general.name')}}</th>
+                                            <th>{{__('general.remove')}}</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <!-- rows -->
+                                        @foreach($course->Students as $student)
+                                            <tr>
+                                                <td>{{$student->neptun}}</td>
+                                                <td>{{$student->name}}</td>
+                                                <td>
+                                                    <button class="btn btn-error btn-sm"
+                                                            wire:click="removeStudent({{$student->id}})">
+                                                        <x-icons.delete_fill_small/>{{__('general.remove')}}</button>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                        <!-- foot -->
+                                        <tfoot>
+                                        <tr>
+                                            <th>{{__('general.neptunCode')}}</th>
+                                            <th>{{__('general.name')}}</th>
+                                            <th>{{__('general.remove')}}</th>
+                                        </tr>
+                                        </tfoot>
+
+                                    </table>
                                 @endif
                             </div>
                         </div>

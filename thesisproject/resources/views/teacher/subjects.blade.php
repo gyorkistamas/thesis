bc58acc1d0f882eb7174c83cf0455af1a79db4c8
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-06 16:34:35 +0100

    add view for teacher subjects

---
+++ thesisproject/resources/views/teacher/subjects.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.default_layout')
+
+@section('title', __('teacher.mySubjectSlashCourses'))
+
+@section('content')
+    <x-rounded-container class="mt-5">
+
+    </x-rounded-container>
+@endsection

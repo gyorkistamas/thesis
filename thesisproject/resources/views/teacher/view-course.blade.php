ddad387a600ed78f03ecb59e46b533f60ae612cb
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-08 15:32:41 +0100

    add view for class

---
+++ thesisproject/resources/views/teacher/view-course.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.default_layout')
+
+@section('title', __('teacher.viewClass'))
+
+@section('content')
+    <x-rounded-container class="mt-5">
+        <livewire:teacher.view-class :class="$courseClass"/>
+    </x-rounded-container>
+@endsection

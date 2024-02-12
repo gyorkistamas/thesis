73704659deada6673d3e649639891a884e1358ea
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-12 19:15:36 +0100

    remove unnecessary code

--- thesisproject/app/Providers/AppServiceProvider.php
+++ thesisproject/app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Broadcasting\Broadcasters\PusherBroadcaster;
+use Illuminate\Broadcasting\BroadcastManager;
 use Illuminate\Support\ServiceProvider;
+use Pusher\Pusher;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
     }
 }

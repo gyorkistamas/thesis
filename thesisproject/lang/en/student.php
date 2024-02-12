f721cb51b31d90b1e2c068aa0be79c4a277c9e37
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-12 17:52:26 +0100

    localization

--- thesisproject/lang/en/student.php
+++ thesisproject/lang/en/student.php
@@ -2,4 +2,7 @@
 
 return [
     'loginToClass' => 'Login to class',
+    'status' => 'Status',
+    'iAmPresent' => 'I am present',
+    'presenceAlreadySetCannotModifyHere' => 'Presence already set, cannot modify here',
 ];
--- thesisproject/lang/hu/student.php
+++ thesisproject/lang/hu/student.php
@@ -2,4 +2,7 @@
 
 return [
     'loginToClass' => 'Bejelentkezés órára',
+    'status' => 'Státusz',
+    'iAmPresent' => 'Jelen vagyok',
+    'presenceAlreadySetCannotModifyHere' => 'Jelenlét már beállítva, itt nem módosítható.',
 ];

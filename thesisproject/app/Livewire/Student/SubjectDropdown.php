601aa19dce9329b34d1f3d7f07784d8eb4d5ea77
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-14 17:15:11 +0100

    subjectvuew controller

---
+++ thesisproject/app/Livewire/Student/SubjectDropdown.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire\Student;
+
+use Livewire\Component;
+
+class SubjectDropdown extends Component
+{
+    public $subject;
+
+    public $semesterSearch;
+
+    public function mount($subject, $semesterSearch)
+    {
+        $this->subject = $subject;
+        $this->semesterSearch = $semesterSearch;
+    }
+
+    public function render()
+    {
+        return view('livewire.student.subject-dropdown');
+    }
+}

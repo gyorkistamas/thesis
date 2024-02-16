ff430fa0d06ca79866ac8c0db6017bdef5ab111c
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-16 15:47:33 +0100

    add delete function

--- thesisproject/app/Livewire/Student/Justifications/JustificationItem.php
+++ thesisproject/app/Livewire/Student/Justifications/JustificationItem.php
@@ -4,16 +4,46 @@ namespace App\Livewire\Student\Justifications;
 
 use App\Models\Subject;
 use Livewire\Component;
+use Usernotnull\Toast\Concerns\WireToast;
 
 class JustificationItem extends Component
 {
+    use WireToast;
+
     public $justification;
 
     public $isOpened = false;
 
+    public $deleted = false;
+
     public function mount($justification)
     {
         $this->justification = $justification;
+    }
+
+    public function deleteJustification()
+    {
+        if (\Auth::user()->cannot('delete', $this->justification)) {
+            toast()->danger(__('general.noPermission'), __('general.error'))->push();
+
+            return;
+        }
+
+        foreach ($this->justification->Pictures as $picture) {
+            $path = public_path().'/storage/'.$picture->picture_name;
+            unlink($path);
+            $picture->delete();
+        }
+        foreach ($this->justification->GetTeachers as $response) {
+            $response->pivot->delete();
+        }
+
+        $this->dispatch('closeDeleteModal', ['id' => $this->justification->id]);
+        $temp = $this->justification;
+        $this->justification = null;
+        $this->deleted = true;
+        $temp->delete();
+        toast()->success(__('student.justificationDeleted'), __('general.success'))->push();
     }
 
     public function getAffectedClasses()

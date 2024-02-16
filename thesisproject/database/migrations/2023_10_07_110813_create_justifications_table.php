f2c37c5f15053d8bd09e951fba9fe6c5908e05d4
gyorkistamas <tamasgyorkis@gmail.com>
2024-02-16 15:46:52 +0100

    add nullable description

--- thesisproject/database/migrations/2023_10_07_110813_create_justifications_table.php
+++ thesisproject/database/migrations/2023_10_07_110813_create_justifications_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained();
             $table->dateTime('start_date');
             $table->dateTime('end_time');
-            $table->string('description');
+            $table->string('description')->nullable();
         });
     }
 

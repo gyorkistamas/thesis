9c813fba7bd1fd8552f5d414b65f0c6d7a2d900c
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-03 16:58:43 +0100

    add other pwa package

--- thesisproject/config/app.php
+++ thesisproject/config/app.php
@@ -186,7 +186,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
-        'LaravelPwa' => \Ladumor\LaravelPwa\LaravelPwa::class,
+        'DotenvEditor' => \Brotzka\DotenvEditor\DotenvEditorFacade::class,
     ])->toArray(),
 
 ];

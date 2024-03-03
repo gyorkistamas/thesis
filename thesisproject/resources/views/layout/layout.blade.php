87ca3957d0c939611bbd8d3ddc8558bbb0001b1d
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-03 17:00:09 +0100

    add pwa directive

--- thesisproject/resources/views/layout/layout.blade.php
+++ thesisproject/resources/views/layout/layout.blade.php
@@ -8,7 +8,6 @@
     <link rel="icon" type="image/x-icon" href="{{config('presencetracker.favicon')}}">
     <meta name="theme-color" content="#6777ef"/>
     <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
-    <link rel="manifest" href="{{route('manifest')}}">
     @vite('resources/css/app.css')
     @vite('resources/css/themeswitcher.css')
     @vite('resources/js/app.js')
@@ -16,28 +15,14 @@
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/locales/hu.js"></script>
     @yield('styles')
+
+    @laravelPWA
+
 </head>
 <body class="font-serif h-full min-h-screen" dir="ltr">
 <livewire:toasts class="z-[900]"/>
 @yield('content_place')
 
 @yield('scripts')
-<script src="{{ asset('/sw.js') }}" data-navigate-once></script>
-<script data-navigate-once>
-    if ("serviceWorker" in navigator) {
-        // Register a service worker hosted at the root of the
-        // site using the default scope.
-        navigator.serviceWorker.register("/sw.js").then(
-            (registration) => {
-                console.log("Service worker registration succeeded:", registration);
-            },
-            (error) => {
-                console.error(`Service worker registration failed: ${error}`);
-            },
-        );
-    } else {
-        console.error("Service workers are not supported.");
-    }
-</script>
 </body>
 </html>

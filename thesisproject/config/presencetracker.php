59e44669cdbe977498072ebbc799303c9bae1121
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-03 12:46:06 +0100

    add config for neptun codes

--- thesisproject/config/presencetracker.php
+++ thesisproject/config/presencetracker.php
@@ -6,4 +6,6 @@ return [
     'logo' => env('LOGO', '/def_logo.png'),
     'enableRegister' => env('ALLOWREGISTER', true),
     'maxNotJustifiedAbsences' => env('MAX_NOT_JUSTIFIED_ABSENCES', 3),
+    'allowChangeNeptunCode' => env('ALLOW_CHANGE_NEPTUN_CODE', false),
+    'requireNeptunCode' => env('REQUIRE_NEPTUN_CODE', false),
 ];

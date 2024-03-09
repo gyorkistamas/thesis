d4ce6ed7cd3912634f3c51d72dadcea514fdbf1e
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-09 14:25:00 +0100

    add install scripts

---
+++ thesisproject/install_linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+composer install --optimize-autoloader --no-dev
+npm install
+npm run build
+php artisan key:generate
+php artisan migrate --seed
+php artisan storage:link
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
---
+++ thesisproject/install_windows.ps1
@@ -0,0 +1,9 @@
+composer install --optimize-autoloader --no-dev
+npm install
+npm run build
+php artisan key:generate
+php artisan migrate --seed
+php artisan storage:link
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache

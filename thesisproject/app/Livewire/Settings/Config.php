6f4837b04118984dc261f2be4dc05f681a59bf6a
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-09 13:32:18 +0100

    change config for superadmin

--- thesisproject/app/Livewire/Settings/Config.php
+++ thesisproject/app/Livewire/Settings/Config.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire\Settings;
 
+use Auth;
 use Brotzka\DotenvEditor\DotenvEditor;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Usernotnull\Toast\Concerns\WireToast;
 
 class Config extends Component
 {
-    private $editor;
+    use WireToast, WithFileUploads;
 
     public $siteName;
 
@@ -21,13 +24,52 @@ class Config extends Component
 
     public function mount()
     {
-        $this->editor = new DotenvEditor();
-
         $this->siteName = env('SITENAME');
         $this->allowRegister = env('ALLOWREGISTER');
         $this->logo = env('LOGO');
         $this->allowChange = env('ALLOW_CHANGE_NEPTUN_CODE');
         $this->requireNeptun = env('REQUIRE_NEPTUN_CODE');
+    }
+
+    public function save()
+    {
+        if (! Auth::user()->hasRole('superadmin')) {
+            toast()->danger(__('general.noPermission'), __('general.error'))->push();
+
+            return;
+        }
+
+        $this->validate([
+            'siteName' => 'required|string|max:255',
+            'allowRegister' => 'bool',
+            'logo' => 'nullable',
+            'allowChange' => 'bool',
+            'requireNeptun' => 'bool',
+        ]);
+
+        $editor = new DotenvEditor();
+
+        toast()->success(__('config.changesSavedReload'), __('general.success'))->push();
+        if ($this->logo != $editor->getValue('LOGO')) {
+            $this->logo = $this->logo->store('logos', 'public');
+            $this->logo = 'storage/'.str_replace('public/', '', $this->logo);
+
+            $editor->changeEnv([
+                'SITENAME' => '"'.$this->siteName.'"',
+                'ALLOWREGISTER' => $this->allowRegister ? 'true' : 'false',
+                'LOGO' => '"'.$this->logo.'"',
+                'ALLOW_CHANGE_NEPTUN_CODE' => $this->allowChange ? 'true' : 'false',
+                'REQUIRE_NEPTUN_CODE' => $this->requireNeptun ? 'true' : 'false',
+            ]);
+        } else {
+            $editor->changeEnv([
+                'SITENAME' => '"'.$this->siteName.'"',
+                'ALLOWREGISTER' => $this->allowRegister ? 'true' : 'false',
+                'ALLOW_CHANGE_NEPTUN_CODE' => $this->allowChange ? 'true' : 'false',
+                'REQUIRE_NEPTUN_CODE' => $this->requireNeptun ? 'true' : 'false',
+            ]);
+        }
+
     }
 
     public function render()
--- thesisproject/resources/views/livewire/settings/config.blade.php
+++ thesisproject/resources/views/livewire/settings/config.blade.php
@@ -4,4 +4,68 @@
     </div>
 
     <div class="divider mt-1 mb-1"></div>
+
+    <div class="flex flex-col items-center ">
+        <div>
+            <label class="input input-bordered input-accent flex items-center gap-2">
+                {{__('general.siteName')}}
+                <input type="text" class="grow" wire:model="siteName"/>
+            </label>
+            @error('siteName')
+            <x-error-alert class="mt-3">
+                {{$message}}
+            </x-error-alert>
+            @enderror
+        </div>
+
+        <div class="form-control w-72 mt-4">
+            <label class="cursor-pointer label">
+                <span class="label-text">{{__('general.allowRegister')}}</span>
+                <input type="checkbox" class="toggle toggle-accent" wire:model="allowRegister" />
+                @error('allowRegister')
+                <x-error-alert>
+                    {{$message}}
+                </x-error-alert>
+                @enderror
+            </label>
+        </div>
+
+        <div class="form-control w-72 mt-4">
+            <label class="cursor-pointer label">
+                <span class="label-text">{{__('config.allowChangeNeptunCode')}}</span>
+                <input type="checkbox" class="toggle toggle-accent" wire:model="allowChange" />
+                @error('allowChange')
+                <x-error-alert>
+                    {{$message}}
+                </x-error-alert>
+                @enderror
+            </label>
+        </div>
+
+        <div class="form-control w-72 mt-4">
+            <label class="cursor-pointer label">
+                <span class="label-text">{{__('config.requireNeptunCode')}}</span>
+                <input type="checkbox" class="toggle toggle-accent" wire:model="requireNeptun" />
+                @error('requireNeptun')
+                <x-error-alert>
+                    {{$message}}
+                </x-error-alert>
+                @enderror
+            </label>
+        </div>
+
+        <label class="form-control w-72 mt-4">
+            <div class="label">
+                <span class="label-text">{{__('config.sitePicture')}}</span>
+            </div>
+            <input type="file" class="file-input file-input-bordered file-input-accent w-full max-w-xs" wire:model="logo" />
+            @error('logo')
+            <x-error-alert>
+                {{$message}}
+            </x-error-alert>
+            @enderror
+        </label>
+
+        <button class="btn btn-success mt-4 w-72" wire:click="save"><x-icons.plus_fill_small />{{__('general.save')}}</button>
+    </div>
 </div>

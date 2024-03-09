b5c9788be919c9b1dc684b13afd5ef1529038fa0
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-09 13:32:22 +0100

    localization

--- thesisproject/lang/en/config.php
+++ thesisproject/lang/en/config.php
@@ -2,4 +2,8 @@
 
 return [
     'siteConfig' => 'Site configuration',
+    'allowChangeNeptunCode' => 'Allow users to change their Neptun code',
+    'requireNeptunCode' => 'Require Neptun code',
+    'sitePicture' => 'Site picture',
+    'changesSavedReload' => 'Changes are being saved, the site will reload in a few seconds.',
 ];
--- thesisproject/lang/hu/config.php
+++ thesisproject/lang/hu/config.php
@@ -2,4 +2,8 @@
 
 return [
     'siteConfig' => 'Alkalmazás konfiguráció',
+    'allowChangeNeptunCode' => 'A felhasználók megváltoztathatják a Neptun kódjukat',
+    'requireNeptunCode' => 'Neptun kód megadása kötelező',
+    'sitePicture' => 'Alkalmazás képe',
+    'changesSavedReload' => 'A változások mentése folyamatban van, az alkalmazás néhány másodperc múlva újratöltődik.',
 ];

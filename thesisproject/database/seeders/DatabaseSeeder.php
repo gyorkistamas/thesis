59c05336f5b59583d187ec965a8ae1c2c0f9dddb
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-09 13:48:21 +0100

    env example and default user

--- thesisproject/database/seeders/DatabaseSeeder.php
+++ thesisproject/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'basicadmin@basicadmin.com',
+            'password' => bcrypt('admin'),
+            'neptun' => '000000',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user->roles()->updateOrCreate(['role' => 'superadmin']);
     }
 }

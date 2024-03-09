522c9d2f7bd1d586c2eb8588fe06afb8a6b1784d
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-09 15:54:02 +0100

    add default user accounts

--- thesisproject/database/seeders/DatabaseSeeder.php
+++ thesisproject/database/seeders/DatabaseSeeder.php
@@ -14,12 +14,36 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $user = User::create([
-            'name' => 'Admin',
-            'email' => 'basicadmin@basicadmin.com',
-            'password' => bcrypt('admin'),
-            'neptun' => '000000',
+            'name' => 'SuperAdmin',
+            'email' => 'superadmin@presencetracker.com',
+            'password' => bcrypt('superadmin'),
+            'neptun' => 'SADMIN',
         ]);
 
         $user->roles()->updateOrCreate(['role' => 'superadmin']);
+
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@presencetracker.com',
+            'password' => bcrypt('admin'),
+            'neptun' => 'ADMIN0',
+        ]);
+        $user->roles()->updateOrCreate(['role' => 'admin']);
+
+        $user = User::create([
+            'name' => 'Teacher',
+            'email' => 'teacher@presencetracker.com',
+            'password' => bcrypt('teacher'),
+            'neptun' => 'TEACHE',
+        ]);
+        $user->roles()->updateOrCreate(['role' => 'teacher']);
+
+        $user = User::create([
+            'name' => 'Student',
+            'email' => 'student@presencetracker.com',
+            'password' => bcrypt('student'),
+            'neptun' => 'STUDEN',
+        ]);
+        $user->roles()->updateOrCreate(['role' => 'student']);
     }
 }

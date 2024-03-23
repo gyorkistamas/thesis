3d3974007d0c84c42630332eaa6afaa2bfdd6ee9
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-23 18:34:33 +0100

    change icon

--- thesisproject/resources/views/layout/default_layout.blade.php
+++ thesisproject/resources/views/layout/default_layout.blade.php
@@ -104,10 +104,8 @@
                         <div class="dropdown dropdown-end align-top">
                             <a tabindex="0" class="btn btn-ghost btn-circle avatar" href="/login" wire:navigate>
                                 <div class="w-10 rounded-full">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="stroke-neutral">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="stroke-success">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
                                     </svg>
                                 </div>
                             </a>

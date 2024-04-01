4d3e6f407500f26a1f4624a95a7c3aca04f0090d
gyorkistamas <tamasgyorkis@gmail.com>
2024-04-01 15:45:01 +0200

    thesis

--- documents/codes/migration.php
+++ documents/codes/migration.php
@@ -3,7 +3,7 @@ Schema::create('courses', function (Blueprint $table) {
     $table->string('course_id');
     $table->string('description')->nullable();
     $table->string('subject_id');
-    $table->foreign('subject_id')->references('id')->on('subjects')->cascadeOnDelete();
+    $table->foreign('subject_id')->references('id')->on('subjects')->constrained()->cascadeOnDelete();
     $table->foreignId('term_id')->nullable()->constrained()->nullOnDelete();
     $table->integer('course_limit')->default(20);
     $table->timestamps();

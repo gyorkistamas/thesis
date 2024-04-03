5c16ec42c3d8b74be0427bff2f9eab2448519630
gyorkistamas <tamasgyorkis@gmail.com>
2024-04-03 12:05:18 +0200

    small design fix

--- thesisproject/resources/views/livewire/landing/student-classes.blade.php
+++ thesisproject/resources/views/livewire/landing/student-classes.blade.php
@@ -8,13 +8,12 @@
             @forelse($classes as $class)
                 <div class="card bg-base-100 mb-2 p-0">
                     <div class="card-body p-1 ps-3 gap-1 ">
-                        <h2 class="card-title">{{$class->Course->Subject->name}} - {{$class->Course->course_id}}
-                            @if($class->isOnGoing())
-                                <div class="badge badge-success gap-2">
+                        @if($class->isOnGoing())
+                            <div class="badge badge-success gap-2">
                                 {{__('general.onGoing')}}
                             </div>
-                            @endif
-                        </h2>
+                        @endif
+                        <h2 class="card-title">{{$class->Course->Subject->name}} - {{$class->Course->course_id}}</h2>
                         <span class="flex flex-row flex-wrap items-center gap-1">
                             <x-icons.time_fill_small />
                             <span class="mr-2">
